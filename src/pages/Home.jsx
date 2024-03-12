16267f0262c54760fac649ee2f78a4974baa50af
Victor Aguado Navarro <aguadonavarrovictor@gmail.com>
2024-03-12 13:11:20 +0100

    Img Fondo Home solucionado. Solo aparece en Home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,7 @@ import { Login } from "../components/Login";
 
 export function Home() {
   return (
-    <div className="w-full flex justify-center">
+    <div className="home-container w-full flex justify-center">
       <div className="inicio-container">
         <Login />
       </div>
