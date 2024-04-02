14cfa91cab6b37a06bce0be06c9706e1e71d8896
Victor Aguado Navarro <aguadonavarrovictor@gmail.com>
2024-04-03 00:47:26 +0200

    titulo plant text-verde

--- src/components/main-content-components/Plantas.jsx
+++ src/components/main-content-components/Plantas.jsx
@@ -3,7 +3,7 @@ import { Card } from "../Card";
 export function Plantas() {
   return (
     <div className="plantas-container my-3">
-      <h1 className="text-start mb-5">Plantas</h1>
+      <h1 className="text-start mb-5 text-verde">Plantas</h1>
 
       <div className="plantas flex justify-center items-start mb-[100px]">
         <Card type="plantas" />
